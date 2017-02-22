097775d7af92f49a19f34f1794bdc8544cc60c46
陈明智 <chenmz@newlandpayment.com>
2017-02-22 17:23:14 +0800

    第三次提交

--- fight/src/main/java/com/auto/auto/MainActivity.java
+++ fight/src/main/java/com/auto/auto/MainActivity.java
@@ -14,7 +14,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
