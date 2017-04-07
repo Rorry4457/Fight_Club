aecd245195c3e05617d836d6c1fe099d17ff9a63
陈明智 <chenmz@newlandpayment.com>
2017-04-07 15:05:35 +0800

    完成登录后finish掉登录页

--- fight/src/main/java/com/auto/auto/LoginActivity.java
+++ fight/src/main/java/com/auto/auto/LoginActivity.java
@@ -56,6 +56,7 @@ public class LoginActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+        finish();
     }
 
     private List<LoginItem> makeLoginInfo() {
